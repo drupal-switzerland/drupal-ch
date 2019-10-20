0cfe7183d0d6d8fe3b2b79b83d063ddb3d55e1f7
Dan Lemon <daniel.lemon@amazee.com>
2019-10-20 23:09:16 +0200

    Fix lin BG Media

---
+++ storybook/twig/components/link-bg-media/link-bg-media.stories.js
@@ -0,0 +1,9 @@
+import { storiesOf } from '@storybook/html';
+import Wrapper from '../../../utils/storybook/decorators/Wrapper';
+
+import LinkBgMedia from './link-bg-media.html.twig';
+import { linkBgMedia } from './link-bg-media.data';
+
+storiesOf('Components/Link BG Media', module)
+  .addDecorator(Wrapper)
+  .add('Link BG Media', () => LinkBgMedia({ ...linkBgMedia }));
