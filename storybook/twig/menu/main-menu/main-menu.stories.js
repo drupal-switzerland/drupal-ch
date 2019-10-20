37c5243a4f79cd1670fabc31e768922d9c4764c9
Dan Lemon <daniel.lemon@amazee.com>
2019-10-20 23:00:36 +0200

    Add main menu via GraphQL to Header

---
+++ storybook/twig/menu/main-menu/main-menu.stories.js
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/html';
+
+import MainMenu from './main-menu.html.twig';
+import { mainMenu } from './main-menu.data';
+
+storiesOf('Components|Menu/Main', module).add('Desktop', () => MainMenu({ mainMenu }));
