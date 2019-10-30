409df6d1c1a6b7c0a9f18d8370ad14865cdc4bcc
Sarah Geissberger <sarah.geissberger@gmail.com>
2019-10-30 13:09:09 +0100

    typography updates

--- storybook/css/typography.stories.js
+++ storybook/css/typography.stories.js
@@ -32,15 +32,15 @@ storiesOf('Shared styles|Global styles', module)
           <p class="light">${lorem}</p>
         </div>
         <div class="two-columns__column story-wrapper">
-          <h1 class="h4"><u>Site styles:</u></h1>
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-          <h4>Heading 4</h4>
-          <h5>Heading 5</h5>
-          <h6>Heading 6</h6>
-          <h6 class="small">Small</h6>
-          <p>${lorem}</p>
+          <h1 class="bold h4"><u>Light:</u></h1>
+          <h1 class="bold">Heading 1</h1>
+          <h2 class="bold">Heading 2</h2>
+          <h3 class="bold">Heading 3</h3>
+          <h4 class="bold">Heading 4</h4>
+          <h5 class="bold">Heading 5</h5>
+          <h6 class="bold">Heading 6</h6>
+          <p class="bold small">I am small size</p>
+          <p class="bold">${lorem}</p>
         </div>
       </div>
     `,
