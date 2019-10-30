1391f91c117f1cc6e3e282f05983b71e742f62ea
Dan Lemon <daniel.lemon@amazee.com>
2019-10-30 14:51:18 +0100

    TextWithImage story update

--- storybook/twig/components/text/text.stories.js
+++ storybook/twig/components/text/text.stories.js
@@ -2,8 +2,9 @@ import { storiesOf } from '@storybook/html';
 import Wrapper from '../../../utils/storybook/decorators/Wrapper';
 
 import Text from './text.html.twig';
-import { text } from './text.data';
+import { text, textWithImage } from './text.data';
 
 storiesOf('Components|Text', module)
   .addDecorator(Wrapper)
-  .add('Text', () => Text({ ...text }));
+  .add('Text', () => Text({ ...text }))
+  .add('Text with Image', () => Text({ ...textWithImage }));
