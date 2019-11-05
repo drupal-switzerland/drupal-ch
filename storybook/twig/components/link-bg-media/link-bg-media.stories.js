277b84caea2db1ab3baa20e6cfdfbcf652c2322d
Vijay Dubb <VD39@users.noreply.github.com>
2019-11-05 19:37:18 +0100

    Update storybook

--- storybook/twig/components/link-bg-media/link-bg-media.stories.js
+++ storybook/twig/components/link-bg-media/link-bg-media.stories.js
@@ -4,6 +4,6 @@ import Wrapper from '../../../utils/storybook/decorators/Wrapper';
 import LinkBgMedia from './link-bg-media.html.twig';
 import { linkBgMedia } from './link-bg-media.data';
 
-storiesOf('Components|BG Media/Link BG Media', module)
+storiesOf('Components|BG Media', module)
   .addDecorator(Wrapper)
   .add('Link BG Media', () => LinkBgMedia({ ...linkBgMedia }));
--- storybook/twig/components/text-bg-media/text-bg-media.stories.js
+++ storybook/twig/components/text-bg-media/text-bg-media.stories.js
@@ -4,6 +4,6 @@ import Wrapper from '../../../utils/storybook/decorators/Wrapper';
 import TextBgMedia from './text-bg-media.html.twig';
 import { textBgMedia } from './text-bg-media.data';
 
-storiesOf('Components|BG Media/Text BG Media', module)
+storiesOf('Components|BG Media', module)
   .addDecorator(Wrapper)
   .add('Text BG Media', () => TextBgMedia({ ...textBgMedia }));
