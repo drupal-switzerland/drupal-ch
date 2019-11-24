6d9b865db2105170f3f7c56c38d05f11dfa8ee3c
Vijay Dubb <VD39@users.noreply.github.com>
2019-11-24 15:41:53 +0100

    Update main menu

--- storybook/twig/menu/main-menu/index.js
+++ storybook/twig/menu/main-menu/index.js
@@ -1,19 +1,69 @@
 import { LitElement, html, customElement } from 'lit-element';
 
+/**
+ * Will add events to show/hide menu, and to show/hide overlay.
+ */
 class MainMenu extends LitElement {
+  static get properties() {
+    return {
+      openMenu: String,
+      closeMenu: String,
+    };
+  }
+
+  constructor() {
+    super();
+    this.button = null;
+  }
+
   connectedCallback() {
     super.connectedCallback();
-    const toggleMenuButton = this.querySelector('.main-menu button');
-    const menu = this.querySelector('.main-menu ul');
+    this.button = this.querySelector('button.main-menu__action');
 
-    toggleMenuButton.addEventListener('click', () => {
-      const open = JSON.parse(toggleMenuButton.getAttribute('aria-expanded'));
-      toggleMenuButton.setAttribute('aria-expanded', !open);
-      menu.hidden = !menu.hidden;
-      // eslint-disable-next-line no-console
-      console.log('clicked');
-    });
+    if (this.button) {
+      this.button.addEventListener('click', this.toggleMenu);
+    }
   }
+
+  /**
+   * Will set accessibility attributes to the button and set body overflow.
+   * @param {Boolean} isVisible // if the current menu is visible or not
+   */
+  setAttributes = (isVisible = false) => {
+    const title = this.button.querySelector('.visually-hidden');
+    this.button.setAttribute('aria-pressed', isVisible);
+    this.button.setAttribute('aria-expanded', isVisible);
+    this.button.title = isVisible ? this.closeMenu : this.openMenu;
+    title.innerText = isVisible ? this.closeMenu : this.openMenu;
+    document.documentElement.style.overflow = isVisible ? 'hidden' : 'auto';
+  };
+
+  /**
+   * Will show/hide menu, add/remove keydown event, and set attributes.
+   */
+  toggleMenu = () => {
+    if (this.classList.contains('visible')) {
+      this.classList.remove('visible');
+      this.setAttributes();
+      document.removeEventListener('keydown', this.toggleMenuKeyDown);
+    } else {
+      this.classList.add('visible');
+      this.setAttributes(true);
+      document.addEventListener('keydown', this.toggleMenuKeyDown);
+    }
+  };
+
+  /**
+   * Will close the menu only if the Esc key is pressed,
+   * added for accessibility.
+   * @param {Event} event // DOM event
+   */
+  toggleMenuKeyDown = event => {
+    const keyCode = event.keyCode || event.which;
+    if (keyCode === 27) {
+      this.toggleMenu();
+    }
+  };
 
   render() {
     return html`
@@ -22,4 +72,4 @@ class MainMenu extends LitElement {
   }
 }
 
-customElement('responsive-menu')(MainMenu);
+customElement('ds-main-menu')(MainMenu);
