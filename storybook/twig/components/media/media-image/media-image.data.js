4d8465640933c1fede3c9ba8c1b88918f3fc99fc
Dan Lemon <daniel.lemon@amazee.com>
2019-12-02 17:02:57 +0100

    Update mock data, remove responsive_image and graphql_views

--- storybook/twig/components/media/media-image/media-image.data.js
+++ storybook/twig/components/media/media-image/media-image.data.js
@@ -4,25 +4,50 @@ export const mediaImage = {
     mediaImage: {
       url: 'https://via.placeholder.com/640x360?text=16:9',
       alt: 'This is an alt text.',
-      rectangularLarge: {
-        url: 'https://via.placeholder.com/1000x650',
+      rect16x9Large: {
+        url: 'https://via.placeholder.com/1350x2400',
+        height: 1350,
+        width: 2400,
+      },
+      rect16x9Medium: {
+        url: 'https://via.placeholder.com/900x1600',
         height: 900,
         width: 1600,
       },
-      squareLarge: {
-        url: 'https://via.placeholder.com/1000x650',
-        height: 900,
+      rect16x9Small: {
+        url: 'https://via.placeholder.com/450x800',
+        height: 450,
+        width: 800,
+      },
+      rect9x16Large: {
+        url: 'https://via.placeholder.com/2400x1350',
+        height: 2400,
+        width: 1350,
+      },
+      rect9x16Medium: {
+        url: 'https://via.placeholder.com/1600x900',
+        height: 1600,
         width: 900,
       },
-      squareMedium: {
-        url: 'https://via.placeholder.com/1000x650',
-        height: 450,
+      rect9x16Small: {
+        url: 'https://via.placeholder.com/800x450',
+        height: 800,
         width: 450,
       },
+      squareLarge: {
+        url: 'https://via.placeholder.com/1200x1200',
+        height: 1200,
+        width: 1200,
+      },
+      squareMedium: {
+        url: 'https://via.placeholder.com/600x600',
+        height: 600,
+        width: 600,
+      },
       squareSmall: {
-        url: 'https://via.placeholder.com/1000x650',
-        height: 200,
-        width: 200,
+        url: 'https://via.placeholder.com/300x300',
+        height: 300,
+        width: 300,
       },
     },
   },
