52f81e2958e29e789abcd87ac8910c745491be88
Vijay Dubb <VD39@users.noreply.github.com>
2019-12-08 12:14:18 +0100

    Add shadows to storybook, update overlay to have a darker background colour, and make image title font responsive on smaller devices

---
+++ storybook/css/_shadows.stories.js
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/html';
+import Wrapper from '../utils/storybook/decorators/Wrapper';
+
+const shadows = ['shadow-40', 'shadow-70', 'shadow-inset-white'];
+
+storiesOf('Shared styles|Global styles', module)
+  .addDecorator(Wrapper)
+  .add('Shadows', () => {
+    let shadowHTML = '';
+    shadows.forEach(shadow => {
+      shadowHTML += `
+      <div class="item">
+        <div class="shadow ${shadow}"></div>
+        <p class="label p">${shadow}</p>
+      </div>
+    `;
+    });
+
+    return `
+      <div class="shadow-wrapper">
+        ${shadowHTML}
+      </div>
+    `;
+  });
