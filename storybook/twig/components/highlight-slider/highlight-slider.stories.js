bfadafdb720b70f489d75644c95a3fa8fb8b2fc5
Dan Lemon <daniel.lemon@amazee.com>
2020-10-19 16:20:48 +0200

    composer upgrade and add initial forum template

--- storybook/twig/components/highlight-slider/highlight-slider.stories.js
+++ storybook/twig/components/highlight-slider/highlight-slider.stories.js
@@ -4,6 +4,6 @@ import Wrapper from '../../../utils/storybook/decorators/Wrapper';
 import HighlightSlider from './highlight-slider.html.twig';
 import { highlightSlider } from './highlight-slider.data';
 
-storiesOf('Components|Highligh slider', module)
+storiesOf('Components|Highlight slider', module)
   .addDecorator(Wrapper)
   .add('Default', () => HighlightSlider({ ...highlightSlider }));
